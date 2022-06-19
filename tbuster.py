726265d6ed734befbb4aa0f0618308e657c25730
PRINCE KUMAR <56459297+princekrvert@users.noreply.github.com>
2022-06-19 18:42:50 +0530

    Add files via upload
    Tbuster first commit...

--- tbuster.py
+++ tbuster.py
@@ -27,8 +27,28 @@ def isvalid(url):
 def getip(url):
     # first remove the https or http for the usr 
     hlist = url.split("//")
-    ip = socket.gethostbyname(hlist[1])
-    return ip
+    try:
+        ip = socket.gethostbyname(hlist[1])
+        return ip
+    except socket.gaierror:
+        return "Not foumd"
+# Make a function to make requests..
+def makereq(path,url):
+    with open(path,"r") as f:
+        for line in f.readlines():
+            #make url 
+            p_list = line.strip()
+            f_url = f"{url}/{p_list}"
+            #print(f"\033[97;1m \r Checking.. {p_list}")
+            req = requests.get(f_url)
+            if req.status_code == 200:
+                print(f" {f_url} Code:<{req.status_code}> ")
+            elif req.status_code == 301:
+                print(f" {f_url} Code:<{req.status_code}> ")
+            elif req.status_code == 302:
+                print(f" {f_url} Code:<{req.status_code}> ")
+            else:
+                pass
 # handle the arguments of the user 
 if sys.argv[1] == "-h" or sys.argv[1] == "--help":
     help()
@@ -36,13 +56,20 @@ elif isvalid(sys.argv[1]):
     if "-w" in sys.argv :
         print("\033[0;1m [~] Checking wordlist path")
         if os.path.exists(sys.argv[3]):
-            banner()  
+            banner() 
+            ip = getip(sys.argv[1])
+            path = sys.argv[3]
+            print(" ")
+            print(f"[-] {sys.argv[1]} : {ip}")
+            makereq(path,sys.argv[1])
     else:
         print("\033[35;1m [~] Using default passlist ")
         if os.path.exists("test.txt"):
             banner()
             ip = getip(sys.argv[1])
+            print(f"\033[33;1m [~] Using default wordlist \033[36;1m ")
             print(f"[-] {sys.argv[1]} : {ip}")
+            makereq("test.txt",sys.argv[1])
         else :
             print("\033[31;1m File not found")
 else:
